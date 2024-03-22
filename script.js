2c7d6f2437bf9da3f2e58545425e17071b022de0
ShivamJoshii <129366041+ShivamJoshii@users.noreply.github.com>
2024-03-22 14:37:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Smooth scroll for links with hashes
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
